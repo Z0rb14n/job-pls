e2cdb89b9b2f4db35c829ce7ded40926723abf75
Z0rb14n <babybluewoof@gmail.com>
2024-02-19 23:13:41 -0800

    import ink

---
+++ Assets/Editor/EditorTemplate.cs
@@ -0,0 +1,33 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace CEditor
+{
+    /// <summary>
+    /// Custom inspector for -class_name-
+    /// </summary>
+//[CustomEditor(typeof(-class_name-))/*, CanEditMultipleObjects*/] 
+    public class EditorTemplate : UnityEditor.Editor
+    {
+        // ReSharper disable once UnusedMember.Local
+        private static readonly string[] DontInclude = new string[] {"m_Script"};
+
+        /// <summary>
+        /// Runs upon it being shown in editor
+        /// </summary>
+        public override void OnInspectorGUI()
+        {
+            EditorGUI.BeginChangeCheck();
+            // If not editing multiple object, one can just do script = (class_name) target;
+            // DrawPropertiesExcluding(serializedObject, dontInclude);
+            // class_name[] scripts = new class_name[targets.Length];
+            if (GUILayout.Button("Save"))
+            {
+                // perform code upon button  press  
+            }
+
+            // for (int i = 0; i < targets.Length; i++) scripts[i] = (class_name)targets[i];
+            if (EditorGUI.EndChangeCheck()) serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
